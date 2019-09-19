f4a4e271fac8bccdb55e85e6add17e58d912b53d
NotAReliableSource <chris.escobar12@gmail.com>
2019-09-19 12:17:12 -0500

    Update proj1.asm
    Project works for all parts (except extra credit) and meets 15 instruction max requirement

--- proj1.asm
+++ proj1.asm
@@ -1,17 +1,28 @@
-# Var A = $9 (1-100)
-# Var B = $8 = 0xFA19E366
-# addr = $10
-# loopCount = $11
-# Var C = $12
-# temp1 = $14
-# temp2 = $15
-# temp3 = $16
-# Var Ax = $17
-# MultFoldCount = $13
-# largest C = $18
-# div4Loop = $20
+# Author: Chris Escobar
+# ECE 366 Fall 2019 Project 1
+# Multiply and Fold hash function
 
-#A
+
+#Part A
+
+
+# $8 = B = 0xFA19E366
+# $9 = A (1-100)
+# $10 = addr
+# $11 = loopCount (100)
+# $12 = C
+# $13 = MultFold Couter
+# $14 = temp1
+# $15 = temp2
+# $16 = temp3
+# $17 = Ax
+# $18 = largest C
+# $19
+# $20 = div4loop
+# $21
+# $22
+# $23 
+
 lui $8, 0xFA19
 ori $8, $8, 0xE366
 
@@ -55,19 +66,25 @@ loop_100:
 bne $11, $0, loop_100
 
 
-# highest = $8
-# 100 loop count down = $9
-# addr = $10
-# highest addr = $11
-# 4 check loop = $15
-# 4 divide result = $16
-# slt = $17
-# srl/sll = $18
-# final addr = $19
-# final addr addr = $20
 
+# Part B(i)
 
-#Bi
+# $8 = highest
+# $9 = 100 loop counter down
+# $10 = addr
+# $11 = highest addr
+# $12
+# $13
+# $14
+# $15 = 4 check loop
+# $16 = 4 divide result
+# $17 = slt
+# $18 = srl/sll
+# $19 = final addr
+# $20 = final addr addr
+# $21
+# $22
+# $23 
 
 addi $9, $0, 100
 addi $15, $0, 2
@@ -81,10 +98,10 @@ loop_2:
 	lbu $18, 0($10)
 
 	slt $17, $18, $8	#if new bit($18) < highest($8), then $17=1
-	bne $17, $0, skip	#skips is $17 != 0
+	bne $17, $0, skip	#skips if $17 != 0
 
-	add $8, $18, $0
-	add $11, $10, $0
+	addi $8, $18, 0
+	addi $11, $10, 0
 
 	skip:
 	addi $9, $9, -1		# increment 100 loop counter
@@ -93,13 +110,16 @@ loop_2:
 	bne $9, $0, loop_2
 
 sb $8, 0($19)
-sw $11, 0($20)	
+sb $11, 0($20)
+srl $11, $11, 8
+addi $20, $20, 1
+sb $11, 0($20)
 
 
 
-#Bii
+#Part B(ii)
 
-# $8 = B
+# $8
 # $9 = 100 loop counter
 # $10 = addr looper
 # $11 = 1 and check bit
@@ -117,7 +137,6 @@ sw $11, 0($20)
 # $23 = total 11111 count
 
 
-
 ori $10, $0, 0x2020
 addi $9, $0, 100
 addi $11, $0, 1
@@ -133,116 +152,111 @@ loop_bii:
 	loop_bit:
 		addi $16, $12, 0
 		
-		divu $17, $18
-		mfhi $13
+		multu $17, $18
+		mflo $13
 		
 		ori $14, $0, 0
 		beq $13, $14, first_bit
 		
-		ori $14, $0, 1
+		ori $14, $0, 8
 		beq $13, $14, second_bit
 		
-		ori $14, $0, 2
+		ori $14, $0, 16
 		beq $13, $14, third_bit
 		
-		ori $14, $0, 3
+		ori $14, $0, 24
 		beq $13, $14, fourth_bit
 		
-		ori $14, $0, 4
+		ori $14, $0, 32
 		beq $13, $14, fifth_bit
 		
-		ori $14, $0, 5
+		ori $14, $0, 40
 		beq $13, $14, sixth_bit
 		
-		ori $14, $0, 6
+		ori $14, $0, 48
 		beq $13, $14, seventh_bit
 		
-		ori $14, $0, 7
+		ori $14, $0, 56
 		beq $13, $14, eighth_bit
 				
 		first_bit:
-		sll $16, $16, 31	# 1
-		srl $16, $16, 31
-		and $19, $16, $11
-		j end_check
+			sll $16, $16, 31	# 1
+			srl $16, $16, 31
+			and $19, $16, $11
+		beq $0, $0 end_check
 		
 		second_bit:
-		sll $16, $16, 30	# 2
-		srl $16, $16, 31
-		and $19, $16, $11
-		j end_check
+			sll $16, $16, 30	# 2
+			srl $16, $16, 31
+			and $19, $16, $11
+		beq $0, $0 end_check
 		
 		third_bit:
-		sll $16, $16, 29	# 3
-		srl $16, $16, 31
-		and $19, $16, $11
-		j end_check
+			sll $16, $16, 29	# 3
+			srl $16, $16, 31
+			and $19, $16, $11
+		beq $0, $0 end_check
 		
 		fourth_bit:
-		sll $16, $16, 28	# 4
-		srl $16, $16, 31
-		and $19, $16, $11
-		j end_check
+			sll $16, $16, 28	# 4
+			srl $16, $16, 31
+			and $19, $16, $11
+		beq $0, $0 end_check
 		
 		fifth_bit:
-		sll $16, $16, 27	# 5
-		srl $16, $16, 31
-		and $19, $16, $11
-		j end_check
+			sll $16, $16, 27	# 5
+			srl $16, $16, 31
+			and $19, $16, $11
+		beq $0, $0 end_check
 		
 		sixth_bit:
-		sll $16, $16, 26	# 6
-		srl $16, $16, 31
-		and $19, $16, $11
-		j end_check
+			sll $16, $16, 26	# 6
+			srl $16, $16, 31
+			and $19, $16, $11
+		beq $0, $0 end_check
 		
 		seventh_bit:
-		sll $16, $16, 25	# 7
-		srl $16, $16, 31
-		and $19, $16, $11
-		j end_check
+			sll $16, $16, 25	# 7
+			srl $16, $16, 31
+			and $19, $16, $11
+		beq $0, $0 end_check
 		
 		eighth_bit:
-		sll $16, $16, 24	# 8
-		srl $16, $16, 31
-		and $19, $16, $11
-		j end_check
+			sll $16, $16, 24	# 8
+			srl $16, $16, 31
+			and $19, $16, $11
+		beq $0, $0 end_check
 		
 		end_check:
+		beq $19, $0, not_consec
+		addi $15, $15, 1
 		
-		add $15, $15, $19
-		
+		not_consec:
 		beq $15, $21, oneoneone
-		
 		beq $19, $0, cont_reset
-		
-		j no_reset
+		beq $0, $0 no_reset
 		
 		cont_reset:
-		addi $15, $0, 0		#reset
+		addi $15, $0, 0		#resets consecutive counter if bit is zero
 		
 		no_reset:
-		
 		addi $17, $17, 1
 		
 	bne $17, $18, loop_bit
 		
-	
-	j after_one
+	beq $0, $0, after_one
 	
 	oneoneone:
-		addi $23, $23, 1	#oneone
+	addi $23, $23, 1	#oneone
 		
 	after_one:
-	
 	addi $9, $9, -1		#after_one
 	addi $10, $10, 1
 	addi $17, $0, 0
 	addi $15, $0, 0
+	
 bne $9, $0, loop_bii
 	
 ori $14, $0, 0x2008
-sb $11, 0($14)
-	
-	
+sb $23, 0($14)
 
